1125e5d64b779376bd7fd3c07ca35a6bec2e0a1d
Pedro Henrique de Souza <dedecamp06@hotmail.com>
2023-04-18 13:50:30 -0300

    update useCase

--- src/useCase/appUseCase.ts
+++ src/useCase/appUseCase.ts
@@ -1,8 +1,25 @@
+import {
+  Controller,
+  errorResponse,
+  HttpResponse,
+  successResponse,
+} from '@/presentation/contracts';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class appUseCase implements Controller {
+  constructor() { }
+
+  async handle(params: appUseCase.Params): Promise<HttpResponse> {
+    try {
+      const result = 'Hello World'
+      return successResponse(result);
+    } catch (error) {
+      return errorResponse(error);
+    }
   }
+}
+
+export namespace appUseCase {
+  export type Params = any
 }
